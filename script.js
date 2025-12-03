5e5c092d5f27b9ff7fa5c6d9caa37b745be97430
23lisve <h23lisve@du.se>
2025-12-03 12:53:58 +0100

    skapade html och scrip filerna

---
+++ script.js
@@ -0,0 +1,37 @@
+const checkboxElement = document.getElementById("divStyle");
+
+const textfieldElement = document.getElementsByClassName("textfield");
+
+const buttonElement = document.querySelector(".button"); 
+
+const divElement = document.querySelector("#div"); 
+
+
+
+/*textfieldElement.onclick = handleClick;
+textfieldElement.oninput = function() {console.log('Något skrivdes!');};
+textfieldElement.addEventListener()
+
+function handleWorth (e){
+console.log(e.target)
+if (e.target.name === 'content' )
+    print(e.target);
+    divElement.innerHTML = e.target
+}
+
+handleWorth ('Hej hej');
+
+
+let divAttribute = document.querySelector('#div');
+
+
+
+let button = document.querySelector(".button");
+button.onclick = function() {console.log('Knappen klickades!');};
+button.addEventListener("click", () => {console.log('Knappen klickades igen');});*/
+
+
+ checkboxElement.addEventListener() => {
+if checkboxElement = checked 
+
+}
