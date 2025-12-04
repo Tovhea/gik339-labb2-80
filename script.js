b8edef030c0323471e18862266c2a4e13d512531
Tovhea <thubinette@hotmail.com>
2025-12-04 14:40:57 +0100

    klara

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
-const checkboxElement = document.getElementById("divStyle");
+/*uppgift 4*/
 
-const textfieldElement = document.getElementsByClassName("textfield");
+const checkboxElement = document.getElementById("divstyle"); /*hämtar checkboxen genom getElementById, fördel: den är snabbare eftersom den bara letar efter exakt id. nackdelarna: mindre flexibel, kan inte ta klassnamn eller andra selectors. */
 
-const buttonElement = document.querySelector(".button"); 
+const buttonElement = document.querySelector(".button"); /*hämtar knappen, fördel: flexibel eftersom den kan använda både id och klass, nackdel: man kan hämta saker som man egentligen inte hade planerat att spara i variabeln*/
 
-const divElement = document.querySelector("#div"); 
+const divElement = document.querySelector("#div"); /*hämtar diven*/
 
-
-
-function handleWorth (e){
-console.log(e.target)
-if (e.target.name === 'content' )
-    console.log(e.target);
-    divElement.innerHTML = e.target.value
-}
-
-textfieldElement[0].onclick = handleWorth; /* ÄNDRA, ÄR INTE LÖST BRA*/
-/*textfieldElement.oninput = function() {console.log('Något skrivdes!');};
-textfieldElement.addEventListener()
-
-function handleWorth (e){
-console.log(e.target)
-if (e.target.name === 'content' )
-    print(e.target);
-    divElement.innerHTML = e.target
-}
-
-handleWorth ('Hej hej');
+const textfieldElement = document.getElementsByClassName("textfield"); /*hämtar textfälten fördel:enkelt att använda och inbyggda i många moderna browsers, nackdel: de läggs i en lista som kan vara lite svår att veta exakt det elementets position som finns där. */
 
 
-let divAttribute = document.querySelector('#div');
+/*uppgift 5*/
+textfieldElement[1].addEventListener("input", function(e){ /*tar andra i listan av textfälten som är sparade i variabeln, lägger till eventlyssnare och hämtar input från textfältet*/ 
+    console.log(e.target); /*skriver ut i konsollen vilket html element som det är hämtat från */
+    console.log(e.target.value);
+    const nameAttribute = e.target.name; /*skapar variabeln nameAttribute och tilldelar den värdet av namnet på html elementet där händelsen inträffade */
+    if (nameAttribute === "content"){ /*om namnet är "content"*/
+        divElement.innerHTML = e.target.value; /*skrivs det som är inskrivet i textrutan ut i diven*/
+    }
+})
 
+/*uppgift 6*/
 
+textfieldElement[0].addEventListener("input", function(e){ /* hämtar första textfältet, lägger till eventlyssnare och dess input som i tidigare funktion*/
+    const nameAttribute = e.target.name; /*skapar en variabel och tilldelar värdet för namnet av html elementet */
+});
 
-let button = document.querySelector(".button");
-button.onclick = function() {console.log('Knappen klickades!');};
-button.addEventListener("click", () => {console.log('Tack för att du klickade knappen');});
+checkboxElement.addEventListener("change", function(e){ /*hämtar checkboxen och skapar funktion som aktiveras när man klickar på/när det blir en ändring i checkboxen*/ 
+    const divColor = textfieldElement[0].value; /*skapar en variabel som lagrar den färg som anvndaren har skrivit in i fältet*/ 
+    divElement.style.backgroundColor = divColor; /*ändrar färg beroende på användarens input på divelementet*/ 
+})
 
-
- checkboxElement.addEventListener() => {
-if checkboxElement = checked 
-
-}*/
+buttonElement.addEventListener("click", function(){ /*När knappen klickas på*/
+divElement.remove();/*tas div elementet bort*/
+})
